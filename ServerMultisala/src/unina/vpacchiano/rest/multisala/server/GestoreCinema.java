5fafeca775f28a7c1b01c8af952f705dd15e1e8a
Dan <danil.gentile@studenti.unina.it>
2020-08-28 18:32:08 +0200

    push 1

--- ServerMultisala/src/unina/vpacchiano/rest/multisala/server/GestoreCinema.java
+++ ServerMultisala/src/unina/vpacchiano/rest/multisala/server/GestoreCinema.java
@@ -21,9 +21,9 @@ public class GestoreCinema {
 	public static void main(String [] args) throws SQLException, UtenteSconosciutoException, UtenteDuplicatoException, FilmSconosciutoException, FilmDuplicatoException, SalaSconosciutaException, SalaDuplicataException, ProgrammazioneSconosciutaException, ProgrammazioneDuplicataException, PrenotazioneSconosciutaException, PrenotazioneDuplicataException, PostiTerminatiException, ChiaveSconosciutaException{
 		GestoreCinema gest = new GestoreCinema();
 		
-		//Utente u = new Utente("Luigino", "Luigi", "Martire", "luigimartire@jj.it", "luigino17", true);
-		//gest.addUtente(u);
-		//gest.addUtente(new Utente("Lallalapalla", "Laura", "Mancini", "lallapallina@alice.it", "lalla44", false));
+		Utente u = new Utente("Luigino", "Luigi", "Martire", "luigimartire@jj.it", "luigino17", true);
+		gest.addUtente(u);
+		gest.addUtente(new Utente("Lallalapalla", "Laura", "Mancini", "lallapallina@alice.it", "lalla44", false));
 		
 		//Utente utente = gest.getUtenteByChiave("56b211bf00faf47d1c3cb7f28104fc8c");
 		//System.out.println(utente);
